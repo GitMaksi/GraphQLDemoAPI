c696310df060b7386b0bbb28db99a4e53d828b36
Maks Sudoł <maksssudol@gmail.com>
2024-03-23 17:15:43 +0100

    secret fix

--- Services/ServiceBusSender.cs
+++ Services/ServiceBusSender.cs
@@ -9,7 +9,7 @@ namespace GraphQLDemoAPI.Services
     {
         public async Task SendMessageAsync<T>(T serviceBusMessage) where T : IServiceBusMessage
         {
-            var client = new ServiceBusClient("Endpoint=sb://ecommerce-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=iYwAzEsh9vlJtdxUp/wPgD9L9kOIDE6Sd+ASbGEqLzQ=");
+            var client = new ServiceBusClient("");
             var sender = client.CreateSender("createproduct");
 
 
